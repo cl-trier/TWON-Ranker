d4336bade3eda77a0ecf39520b82c2d4d2916107
Münker, Simon <muenker@uni-trier.de>
2024-02-16 16:41:58 +0100

    restructure api init

--- api/__init__.py
+++ api/__init__.py
@@ -1,25 +1,36 @@
 from typing import List, Dict
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src import Ranker, Response
 from src.schemas import User, Post
 from .config import Config
 
+cfg = Config()
+
+ranker = Ranker(log_path=cfg.log_path)
+
 app = FastAPI(
-    title=Config.title,
-    description=open(f'{Config.docs_path}/index.md').read(),
-    version=Config.version,
+    title=cfg.title,
+    description=open(f'{cfg.docs_path}/index.md').read(),
+    version=cfg.version,
     docs_url='/'
 )
 
-ranker = Ranker(log_path=Config.log_path)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=cfg.trust_origins,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post(
     "/rank/",
     summary='todo',
-    description=open(f'{Config.docs_path}/rank.md').read()
+    description=open(f'{cfg.docs_path}/rank.md').read()
 )
 async def rank(
         user: User, posts: List[Post], weights: Dict[str, float]
