a247d3a2a05d662844eec6c8c6257048a31b0894
Münker, Simon <muenker@uni-trier.de>
2024-02-24 11:55:56 +0100

    add prelim engagement tests

---
+++ tests/test_engagement.py
@@ -0,0 +1,24 @@
+import datetime
+import math
+import typing
+
+from src import modules
+
+REFERENCE_DATETIME: datetime.datetime = datetime.datetime.now()
+REFERENCE_TIMEDELTA: datetime.timedelta = datetime.timedelta(days=3)
+N: int = 1_000
+
+DATETIME_NOW = datetime.datetime.now()
+
+OBSERVATIONS: typing.List[datetime.datetime] = [
+    REFERENCE_DATETIME - (REFERENCE_TIMEDELTA * i / N)
+    for i in reversed(range(N))
+]
+
+
+def test_count_abs():
+    assert modules.Engagement(func='count_based', log_normalize=False)(items=OBSERVATIONS) == N
+
+
+def test_count_log():
+    assert modules.Engagement(func='count_based', log_normalize=True)(items=OBSERVATIONS) == math.log(N)
